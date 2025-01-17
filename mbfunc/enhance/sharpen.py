e693287143eddc373eca4ac88b1bb987471f38f1
meibanf <123319596+meibanf@users.noreply.github.com>
2025-01-17 10:17:45 -0800

    Update sharpen.py

--- mbfunc/enhance/sharpen.py
+++ mbfunc/enhance/sharpen.py
@@ -1,10 +1,12 @@
+import math
+
 import vapoursynth as vs
 
 core = vs.core
 
 """
 Enhance high-frequency cotent
-(steal from mawen1250's script)
+(Steal from mawen1250's script)
 """
 
 def mwenhance(
@@ -15,7 +17,8 @@ def mwenhance(
     spwr: float = 1/4,
     sdmp_lo: int = 4,
     sdmp_hi: int = 48,
-    soft: float = 0
+    soft: float = 0,
+    expr: bool = True,
 ) -> vs.VideoNode:
 
     if not isinstance(diffclip, vs.VideoNode):
@@ -27,9 +30,9 @@ def mwenhance(
 	# Constants values for sharping lut
     bits = diffclip.format.bits_per_sample
     bps_mul8 = 1 << (bits - 8) 
-    floor_val = 0
-    ceil_val = (1 << bits) - 1
-    neutral_val = 1 << (bits - 1)
+    floor = 0
+    ceil = (1 << bits) - 1
+    neutral = 1 << (bits - 1)
     szrp = szrp8 * bps_mul8
     szrp8_sqr = szrp8 ** 2
     szrp_mul_strength = szrp * strength
@@ -39,37 +42,59 @@ def mwenhance(
     else:
         szrp8_div_sdmp_hi_power4_plus_1 = (szrp8 / sdmp_hi) ** 4 + 1.0
 	
-    # Applying enhanced expr
-    abs_diff_expr = f"x {neutral_val} - abs"
-    abs_diff8_expr = f"x {neutral_val} - {bps_mul8} / abs"
-    diff8_sqr_expr = f"x {neutral_val} - {bps_mul8} / x {neutral_val} - {bps_mul8} / *"
-    sign_mul_expr = f"x {neutral_val} - 0 >= 1 -1 ?"
-
-    res1_expr = ( f"{abs_diff_expr} {szrp} / {spwr} pow "
-        f"{szrp_mul_strength} * {sign_mul_expr} *" )
-
-    res2_expr = ( f"{diff8_sqr_expr} "
-        f"{szrp8_sqr_plus_sdmp_lo} * "
-        f"{diff8_sqr_expr} {sdmp_lo} + "
-        f"{szrp8_sqr} * / " )
     
-    if sdmp_hi == 0:
-        res3_expr = "0"
-    else:
-        res3_expr = f"{abs_diff8_expr} {sdmp_hi} / 4 pow"
+    if expr:
+        # Expr version
+        abs_diff_expr = f"x {neutral} - abs"
+        abs_diff8_expr = f"x {neutral} - {bps_mul8} / abs"
+        diff8_sqr_expr = f"x {neutral} - {bps_mul8} / x {neutral} - {bps_mul8} / *"
+        sign_mul_expr = f"x {neutral} - 0 >= 1 -1 ?"
 
-    enhanced_expr = ( f"x {neutral_val} = "
-        f"x {ceil_val} {floor_val} {neutral_val} "
-        f"{res1_expr} "
-        f"{res2_expr} * "
-        f"{szrp8_div_sdmp_hi_power4_plus_1} * "
-        f"1 {res3_expr} + / + max min ?" )
+        res1_expr = ( f"{abs_diff_expr} {szrp} / {spwr} pow "
+            f"{szrp_mul_strength} * {sign_mul_expr} *" )
 
-    if diffclip.format.num_planes == 1:
-        enhanced_clip = core.akarin.Expr(diffclip, [expr])
+        res2_expr = ( f"{diff8_sqr_expr} "
+            f"{szrp8_sqr_plus_sdmp_lo} * "
+            f"{diff8_sqr_expr} {sdmp_lo} + "
+            f"{szrp8_sqr} * / " )
+    
+        if sdmp_hi == 0:
+            res3_expr = "0"
+        else:
+            res3_expr = f"{abs_diff8_expr} {sdmp_hi} / 4 pow"
+
+        enhanced_expr = ( f"x {neutral} = "
+            f"x {ceil} {floor} {neutral} "
+            f"{res1_expr} "
+            f"{res2_expr} * "
+            f"{szrp8_div_sdmp_hi_power4_plus_1} * "
+            f"1 {res3_expr} + / + max min ?" )
+    
+        expr = f"x {neutral} = x {enhanced_expr} ?"
+
+        if diffclip.format.num_planes == 1:
+            enhanced_clip = core.akarin.Expr(diffclip, [expr])
+        else:
+            enhanced_clip = core.akarin.Expr(diffclip,[expr, expr if chroma else "",expr if chroma else ""])
+    
     else:
-        enhanced_clip = core.akarin.Expr(diffclip,[expr, expr if chroma else "",expr if chroma else ""])
-	
+        # Orginal lut
+        def enhance_function(x) :
+            abs_diff = abs(x - neutral)
+            abs_diff8 = abs((x - neutral) / bps_mul8)
+            diff8_sqr = ((x - neutral) / bps_mul8) ** 2
+            sign_mul = 1 if x - neutral >= 0 else -1
+
+            res1 = ((abs_diff / szrp) ** spwr) * szrp_mul_strength * sign_mul
+            res2 =  res2 = (diff8_sqr * szrp8_sqr_plus_sdmp_lo) / ((diff8_sqr + sdmp_lo) * szrp8_sqr)
+            res3 = 0 if sdmp_hi == 0 else (abs_diff8 / sdmp_hi) ** 4
+            enhanced = (res1 * res2 * szrp8_div_sdmp_hi_power4_plus_1) / (1 + res3)
+
+            val = round(neutral + enhanced)
+            return min(ceil, max(floor, val))
+        
+        enhanced_clip= diffclip.std.Lut([0,1,2] if chroma else [0], function=enhance_function)
+
     # Optional softening
     if soft > 0:
         softened_clip = core.rgvs.RemoveGrain(enhanced_clip, [19] * enhanced_clip.format.num_planes)
@@ -77,7 +102,7 @@ def mwenhance(
             result = core.std.Merge(enhanced_clip, softened_clip, [soft])
         else:
             result = softened_clip
-        limit_expr = f"x {neutral_val} - abs y {neutral_val} - abs <= x y ?"
+        limit_expr = f"x {neutral} - abs y {neutral} - abs <= x y ?"
         if enhanced_clip.format.num_planes == 1:
             result = core.akarin.Expr([enhanced_clip, result], [limit_expr])
         else:
